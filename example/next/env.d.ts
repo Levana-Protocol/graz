889d4366c11ee4e0a249fbc024b2c70047b0ec09
lvn-skate-dragon <skate-dragon@levana.exchange>
2024-09-26 10:29:57 +0300

    removed monorepo

--- example/next/env.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NEXT_PUBLIC_CAPSULE_ENV?: "DEV" | "SANDBOX" | "BETA" | "PROD";
-  }
-}
--- example/vite/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import nodeStdlibBrowser from "vite-plugin-node-stdlib-browser";
-
-export default defineConfig({
-  plugins: [nodeStdlibBrowser(), react()],
-});
